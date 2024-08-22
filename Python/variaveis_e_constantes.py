115bef1dc4fbdb9b540df44a600a45e2deb602ca
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-21 22:26:30 -0300

    continuacao 21/08

---
+++ Python/variaveis_e_constantes.py
@@ -0,0 +1,10 @@
+#Variaveis
+
+age = 31
+name = 'Giovani de Moura André'
+print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
+
+#Constantes 
+
+BRAZILIAN_STATES = ['SP, MG, RJ']
+print(BRAZILIAN_STATES)
