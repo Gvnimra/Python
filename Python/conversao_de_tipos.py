bb659d9ad8b6fb454a7cee8bd5a2dc8eec3e657c
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-21 22:45:30 -0300

    conversao de tipos (aula)

---
+++ Python/conversao_de_tipos.py
@@ -0,0 +1,50 @@
+#inteiro para float
+print("Inteiro para Float")
+
+preco1 = 10
+print(preco1)
+
+preco1 = float(preco1)
+print(preco1)
+
+preco2 = 10/2
+print(preco2)
+
+#float para inteiro 
+print("Float para Inteiro")
+
+preco3 = 10.30
+print(preco3)
+
+preco3 = int(preco3)
+print(preco3)
+
+#Conversão por divisão 
+print("Conversão por divisão")
+
+preco_divisao = 10
+print(preco_divisao)
+print(preco_divisao / 2)
+print(preco_divisao // 2)
+
+#Numero para String
+print("Numero para String")
+
+valor = 10.50
+idade = 31
+
+print(str(valor))
+print(str(idade))
+
+texto = f"idade {idade} valor {valor}"
+print(idade)
+
+#String para numero 
+print("String para numero")
+
+valor2 = "10.50"
+idade2 = "31"
+
+print(float(valor2))
+print(int(idade2))
+
