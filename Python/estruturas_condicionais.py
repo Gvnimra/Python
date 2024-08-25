ec658bd907f99c628e8f6f8541bfc5cc0992829b
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-25 12:30:51 -0300

    continuacao estruturas condicionais

---
+++ Python/estruturas_condicionais.py
@@ -0,0 +1,14 @@
+#Estruturas Condicionais 
+
+#If
+
+saldo = 2000.0
+saque = float(input("Informe o valor do saque: "))
+
+if saldo >= saque:
+    print("realizando saque!")
+    
+if saldo < saque:
+    print("Saldo insuficiente!")
+
+
