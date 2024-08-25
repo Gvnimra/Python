53b34cd6237178e993d8794325ddf656907aa8bc
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-25 11:42:49 -0300

    continuacao dos estudos

---
+++ Python/operadores_de_associacao.py
@@ -0,0 +1,11 @@
+#Operadores de associacao
+
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+print("Python" in curso)
+
+print("Maçã" not in frutas)
+
+print(200 in saques)
