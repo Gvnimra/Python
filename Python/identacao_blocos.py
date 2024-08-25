baf07d818f6a9227ba4301418aebdaa917285e4a
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-25 12:22:12 -0300

    continuacao

---
+++ Python/identacao_blocos.py
@@ -0,0 +1,18 @@
+#identacao e blocos 
+
+def sacar(valor):
+    saldo = 100
+
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa")
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")    
+
+def depositar(valor):
+    saldo = valor
+    saldo += valor
+
+
+sacar(100)
+
+        
