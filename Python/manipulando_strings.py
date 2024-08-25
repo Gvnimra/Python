d21e97c3bc44e25c6b458932d0be748ffe30a115
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-25 17:52:20 -0300

    continuação dos estudos

---
+++ Python/manipulando_strings.py
@@ -0,0 +1,25 @@
+#Manipulando Strings 
+
+curso = "pYtHoN"
+
+print("Aprendendo manipulação de Strings: Maíuscula, minúscula e título")
+print()
+print(curso)
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+print()
+print("Eliminando espaços em branco")
+print()
+curso_2 = "     Python  "
+
+print(curso_2)
+print(curso_2.strip())
+print(curso_2.lstrip())
+print(curso_2.rstrip())
+
+
+
+
+
+
