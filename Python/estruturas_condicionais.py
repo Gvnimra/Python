ce11c16f25fbff1a53164fb8a34c9ed4194c6745
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-25 13:18:22 -0300

    estruturas condicionais

--- Python/estruturas_condicionais.py
+++ Python/estruturas_condicionais.py
@@ -1,14 +1,47 @@
 #Estruturas Condicionais 
 
-#If
+#If, Elif, Else
 
 saldo = 2000.0
 saque = float(input("Informe o valor do saque: "))
+conta_normal = True
+conta_universitaria = False
+cheque_especial = 150
 
 if saldo >= saque:
     print("realizando saque!")
     
-if saldo < saque:
+else:
     print("Saldo insuficiente!")
+
+#Outro exemplo
+
+opcao = int(input("Informe uma opção: [1] Sacar \n [2] Extrato: "))
+
+if opcao == 1:
+    valor = float(input("informe a quantia para sacar: "))
+
+elif opcao == 2:
+    print("Exibindo o extrato...")
+
+else:
+    SystemExit("Opção inválida")
+
+#If Aninhado 
+
+if conta_normal: 
+    if saldo >= saque: 
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saldo realizado com uso do cheque especial")
+    else:
+        print("Não foi possivel realizar o saque!")
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo insuficiente!")
+
+
 
 
