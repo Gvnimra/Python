28b11cac1ab451ebec065bc6f7a1ac6c3ddd6d55
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-24 22:43:58 -0300

    continuacao dos estudos

--- Python/operadores_aritmeticos.py
+++ Python/operadores_aritmeticos.py
@@ -1,7 +1,7 @@
 #Operadores Aritméticos
 
-produto1 = 100
-produto2 = 20
+produto1 = 20
+produto2 = 10
 
 print(produto1 + produto2)
 print(produto1 - produto2)
@@ -11,5 +11,7 @@ print(produto1 * produto2)
 print(produto1 % produto2)
 print(produto1 ** produto2)
 
-
-
+x = (10 + 5) * 4 
+y = (10 / 2) + (25 * 2) - (2 ** 2)
+print(x)
+print(y)
