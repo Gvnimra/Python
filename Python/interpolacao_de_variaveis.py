03e31cbb600a62038bd46f5fb35571173f31ffc4
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-25 18:39:43 -0300

    strings

---
+++ Python/interpolacao_de_variaveis.py
@@ -0,0 +1,34 @@
+#Interpolaçao de variaveis 
+
+print("INTERPOLACAO DE VARIAVEIS")
+print()
+print("Old Style % ")
+print()
+nome = "Giovani de Moura André"
+idade = 31 
+profissao = "Programador"
+linguagem = "Python"
+
+print("Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e estou estudando %s." %(nome, idade, profissao, linguagem))
+print()
+print("MÉTODO FORMAT")
+print()
+print("Olá, me chamo {}. Eu tenho {} anos de idade, trabalho como {} e estou estudando {}." .format(nome, idade, profissao, linguagem))
+print()
+print("Olá, me chamo {3}. Eu tenho {2} anos de idade, trabalho como {1} e estou estudando {0}." .format(linguagem, profissao, idade, nome))
+print()
+print("Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou estudando {linguagem}." .format(nome = nome, idade = idade, profissao = profissao, linguagem = linguagem))
+print()
+print("Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou estudando {linguagem}." .format(**pessoa))
+print()
+
+print("F-String")
+print()
+print(f"Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou estudando {linguagem}.")
+print()
+PI = 3.14159
+print(f"Valor de PI: {PI: .2f}")
+print(f"Valor de PI: {PI: 10.2f}")
+
+
+
--- Python/manipulando_strings.py
+++ Python/manipulando_strings.py
@@ -1,5 +1,9 @@
 #Manipulando Strings 
 
+print("CONHECENDO MÉTODOS ÚTEIS DA CLASSE STRING!")
+
+print()
+
 curso = "pYtHoN"
 
 print("Aprendendo manipulação de Strings: Maíuscula, minúscula e título")
@@ -12,14 +16,14 @@ print()
 print("Eliminando espaços em branco")
 print()
 curso_2 = "     Python  "
-
+print()
 print(curso_2)
 print(curso_2.strip())
 print(curso_2.lstrip())
 print(curso_2.rstrip())
-
-
-
-
-
+print()
+print("Junções e centralização")
+curso_3 = "Python"
+print(curso_3.center(10, "#"))
+print(".".join(curso_3))
 
