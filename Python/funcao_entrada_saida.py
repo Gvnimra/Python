f76b57446297b45aef967d01ee4e322d6b643232
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-24 22:39:00 -0300

    continuacao dos estudos de Python

---
+++ Python/funcao_entrada_saida.py
@@ -0,0 +1,9 @@
+nome = input("Informe o seu nome: ")
+print("O nome é:",nome)
+
+nome1 = "Giovani"
+sobrenome1 = "Moura"
+
+print(nome1, sobrenome1)
+print(nome1, sobrenome1, end = "...\n")
+print(nome1, sobrenome1, sep="#")
---
+++ Python/operadores_aritmeticos.py
@@ -0,0 +1,15 @@
+#Operadores Aritméticos
+
+produto1 = 100
+produto2 = 20
+
+print(produto1 + produto2)
+print(produto1 - produto2)
+print(produto1 / produto2)
+print(produto1 // produto2)
+print(produto1 * produto2)
+print(produto1 % produto2)
+print(produto1 ** produto2)
+
+
+
