3e238fa903e928dbf2db65ddde5ebf61ec547370
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-24 23:03:05 -0300

    operadoes de atribuicao

---
+++ Python/operadoes_de_atribuicao.py
@@ -0,0 +1,47 @@
+#Operadores de Atribuicao
+
+#atribuicao simples
+
+saldo = 500
+print(saldo)
+
+#atribuicao com adicao
+
+saldoadicao = 500
+saldoadicao += 200
+print(saldoadicao)
+
+#atribuicao com subtracao 
+
+saldosub = 500
+saldosub -= 200
+print(saldosub)
+
+#atribuicao com multiplicacao
+
+saldomult = 500
+saldomult *= 2
+print(saldomult)
+
+#atribuicao com divisao 
+
+saldodiv = 500
+saldodiv/= 2
+print(saldodiv)
+
+saldodiv_2 = 500
+saldodiv_2 //= 5
+print(saldodiv_2)
+
+#atribuicao com modulo
+
+saldomod = 500
+saldomod %= 480
+print(saldomod)
+
+#atribuicao com exponenciacao
+
+saldoexp = 80
+saldoexp **=2
+print(saldoexp)
+
