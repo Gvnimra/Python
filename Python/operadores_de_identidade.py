6a41cc93c09a8d7da61d44ff631346c5aa22603d
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-25 11:27:01 -0300

    continuacao dos estudos

---
+++ Python/operadores_de_identidade.py
@@ -0,0 +1,12 @@
+#Operadores de identidade
+
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200
+
+print(curso is nome_curso)
+
+print(curso is not nome_curso)
+
+print(saldo is limite)
+
