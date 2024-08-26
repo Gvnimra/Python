679e466fcacd4152f1865d79777294b4510efc41
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-26 20:42:46 -0300

    strings

---
+++ Python/string_multiplas_linha.py
@@ -0,0 +1,20 @@
+#Strings em Multiplas Linhas 
+
+print("Strings em multiplas linhas")
+
+nome = "Giovani"
+
+mensagem = f'''
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+'''
+print(mensagem)
+
+print()
+mensagem_2 = f'''
+  Olá meu nome é {nome},
+Eu estou aprendendo Python
+    Essa mensagem tem diferentes recuos :)
+'''
+print(mensagem_2)
+
