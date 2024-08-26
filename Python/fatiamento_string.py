ef5b11c013ccf997ef26e355c3fcc1249f63d4ac
Giovani Moura <thegiovanimoura@gmail.com>
2024-08-26 20:25:40 -0300

    strings

---
+++ Python/fatiamento_string.py
@@ -0,0 +1,13 @@
+#Fatiamento de Strings 
+
+print("FATIAMENTO DE STRINGS")
+
+nome = "Giovani de Moura André"
+
+print(nome[0])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:])
+print(nome[::-1])
