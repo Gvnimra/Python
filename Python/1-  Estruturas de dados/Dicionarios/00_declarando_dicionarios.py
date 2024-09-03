67110930aeb982cf3e1904c57f9fb8aacaa0b65a
Giovani Moura <thegiovanimoura@gmail.com>
2024-09-03 19:09:19 -0300

    dicionarios

---
+++ Python/1-  Estruturas de dados/Dicionarios/00_declarando_dicionarios.py
@@ -0,0 +1,6 @@
+pessoa = {"nome": "Giovani", "idade": 31}
+
+pessoa_dict = dict(nome = "Giovani", idade = 31)
+
+pessoa["telefone"] = "(16)99222-2059"
+
---
+++ Python/1-  Estruturas de dados/Dicionarios/01_acessando_dados.py
@@ -0,0 +1,14 @@
+dados = {"nome": "Giovani", "idade": 31, "telefone": "(16)99222-2059"}
+
+dados["nome"] # "Giovani"
+dados["idade"] # 31
+dados["telefone"] # "(16)99222-2059"
+
+#sobrescrevendo os valores
+
+dados["nome"] = "Maria"
+dados["idade"] = 18
+dados["telefone"] = "(16)99333-3069"
+
+dados #{"nome": "Maria", "idade": 18, "telefone": "(16)99333-3069"}
+ 
