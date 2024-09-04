99ad3a56941a8227d67c2bd799d964a79a66416a
Giovani Moura <thegiovanimoura@gmail.com>
2024-09-04 20:36:58 -0300

    manipulando datas com timedelta

---
+++ Python/4 - Data e Hora/00_datetime.py
@@ -0,0 +1,17 @@
+from datetime import date, datetime, time
+
+data_nasc = date(1993, 2, 12)
+print(data_nasc)
+print('Minha data de nascimento é {}'.format(data_nasc))
+
+print(date.today())
+
+data_hora = datetime(2024, 9, 4, 20, 18)
+print(data_hora)
+print(datetime.today())
+
+hora = time(20,33)
+print(hora)
+
+
+
---
+++ Python/revendo conteudos/1_respondendo_usuario.py
@@ -0,0 +1,2 @@
+nome = input("Por favor, digite o seu nome: ")
+print(f"Olá é um prazer te conhecer, {nome}!")
