06613bc5f74b865b5c117b50ce1fa395c374a896
Giovani Moura <thegiovanimoura@gmail.com>
2024-09-04 23:29:23 -0300

    datetime

--- Python/4 - Data e Hora/02_strftime_strptime.py
+++ Python/4 - Data e Hora/02_strftime_strptime.py
@@ -10,3 +10,14 @@ print(d.strftime("%d/%m/%Y %H:%M"))
 date_string = "04/09/2024 22:25"
 d = datetime.datetime.strptime(date_string, "%d/%m/%Y %H:%M")
 print(d)
+
+data_hora_atual = datetime.now()
+data_hora_str = "2023-10-20 10:20"
+mascara_ptbr = "%d/%m/%Y %a"
+mascara_en = "%Y-%m-%d %H:%M"
+
+print(data_hora_atual.strftime(mascara_ptbr))
+
+data_convertida = datetime.strptime(data_hora_str, mascara_en)
+print(data_convertida)
+print(type(data_convertida))
