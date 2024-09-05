0731f5042fc7bd50d9120c1721e41a302e1fb764
Giovani Moura <thegiovanimoura@gmail.com>
2024-09-05 00:02:44 -0300

    pytz

--- Python/4 - Data e Hora/03_pytz.py
+++ Python/4 - Data e Hora/03_pytz.py
@@ -4,4 +4,16 @@ import pytz
 #criando datetime com timezone
 
 d= datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))
+d2 = datetime.datetime.now(pytz.timezone("Europe/Oslo"))
+
 print(d)
+print(d2)
+
+#criando datetime com timezone
+
+d3 = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-3), "BRT"))
+print(d3)
+
+#convertendo para outro time zone
+d_utc = d.astimezone(datetime.timezone.utc)
+print(d_utc)
