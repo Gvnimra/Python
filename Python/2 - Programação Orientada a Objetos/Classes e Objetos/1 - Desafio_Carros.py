a585a66ef699b21f861d1ade6bb05a148f4d3f6b
Giovani Moura <thegiovanimoura@gmail.com>
2024-09-19 19:27:02 -0300

    POO em python

---
+++ Python/2 - Programação Orientada a Objetos/Classes e Objetos/1 - Desafio_Carros.py
@@ -0,0 +1,8 @@
+class Carro:
+     def __init__(self, cor, modelo, ano, valor):
+        self.cor = cor 
+        self.modelo = modelo 
+        self.ano = ano 
+        self.valor = valor 
+
+volkswagen = Carro("Prata", "Gol", 2024, 35000)
