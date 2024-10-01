49b6d05260ab0a55b1b991e1834d7833fed0c986
Giovani Moura <thegiovanimoura@gmail.com>
2024-09-30 22:38:10 -0300

    Polimorfismo em Python

--- Python/2 - Programação Orientada a Objetos/Polimorfismo/polimorfismo.py
+++ Python/2 - Programação Orientada a Objetos/Polimorfismo/polimorfismo.py
@@ -12,8 +12,18 @@ class Avestruz():
 def plano_de_voo(objeto):
     objeto.voar()
 
+
+class Aviao(Passaro): #exemplo ruim do uso de herança para "ganhar" o método voar
+    def voar(self):
+        print("Avião esta voando...")
+
+
+
+
 pombo = Pombo()
 avestruz = Avestruz()
 
 plano_de_voo(pombo)
 plano_de_voo(avestruz)
+plano_de_voo(Aviao())
+
